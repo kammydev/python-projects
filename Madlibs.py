3098b6009ca2360aacbfbf98e4a7695a11939e6f
kammydev <ezumakamsiyonna@gmail.com>
2024-01-07 11:34:55 +0100

    My first commit - committing all files to the repository

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+noun = input("A noun: ")
+noun2 = input("Another noun: ")
+verb = input("A verb: ")
+verb2 = input("Another verb: ")
+adj = input("An adjective: ")
+adj2 = input("Another adjective: ")
+adj3 = input("Another adjective: ")
+
+
+Madlib = f"Having a {noun} is so {adj}! I can't really explain just how {adj2} it can get \
+to {verb}. I would suggest that anyone who can {verb2} absolutely should and without any \
+{noun2}. It is absolutely {adj3}. "
+
+print(Madlib)
